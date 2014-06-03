bd2ff38dc3bdd70c32eb98264d89a09a605fbd27
ChengduJack <598754264@qq.com>
2014-06-03 14:44:13 +0800

    run oracle script

---
+++ c#/Files/RunScript.aspx
@@ -0,0 +1,231 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="RunScript.aspx.cs" Inherits="RunScript"  %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Run Script page</title>
+    <style>
+        .style1
+        {
+            width: 123px;
+        }
+        .tableborder
+        {
+            border: 0px;
+            border-collapse: collapse;
+            margin: auto;
+            width: 800px;
+        }
+        .tableborder td
+        {
+            border-top: 1px #666 solid;
+            border-right: 1px #666 solid;
+        }
+        .tableborder
+        {
+            border-bottom: 1px #666 solid;
+            border-left: 1px #666 solid;
+        }
+        body
+        {
+            /*background : #b6b7bc;*/
+            font-size: .80em;
+            font-family: "Helvetica Neue" , "Lucida Grande" , "Segoe UI" , Arial, Helvetica, Verdana, sans-serif;
+            margin: 0px;
+            padding: 0px;
+            color: #696969;
+            background-image: url("../Image/blue.png");
+            background-repeat: repeat;
+        }
+        
+        .btn-lit
+        {
+            /* background-image:url(../Image/sea.jpg) ;
+    background-repeat:no-repeat;*/
+            color: White;
+            border: 0px;
+            background: #4b6c9e;
+            padding: 4px;
+            cursor: pointer;
+            font-weight: bold;
+        }
+        .btn-lit:hover
+        {
+            color: yellow;
+        }
+        #GridView1 tr:hover td
+        {
+            background: #F2CA6A;
+        }
+        #GridView1 tr:nth-child(even)
+        {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 11px;
+            font-weight: normal;
+            color: #000000;
+            background-color: #E6E7E8;
+            text-indent: 1px;
+            height: 20px;
+        }
+        
+        .GridRow1
+        {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 11px;
+            font-weight: normal;
+            color: #000000;
+            background-color: #FFFFFF;
+            text-indent: 1px;
+            height: 20px;
+        }
+        .tableHeader
+        {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 11px;
+            font-weight: bold;
+            color: #000000;
+            background-color: #F2CA6A;
+            height: 25px;
+            text-align: left;
+            vertical-align: middle;
+        }
+        .txtbox
+        {
+            -webkit-border-radius: 5px;
+            -moz-border-radius: 5px;
+            border-radius: 5px;
+            background-color: #D7EFFF;
+            border: 1px solid #6297BC;
+            outline: none;
+            padding: 2px 2px;
+            color: #333333;
+            padding: 3px;
+            margin-right: 4px;
+            margin-bottom: 8px;
+            font-family: tahoma, arial, sans-serif;
+        }
+        
+    </style>
+    <script type="text/javascript">
+     
+        function Submit() {
+            var ddlReport = document.getElementById("<%=DropDownList1.ClientID%>");
+
+            var Text = ddlReport.options[ddlReport.selectedIndex].text;
+            if (Text != 'Batch') {
+                var sql = document.getElementById("<%=txtContent.ClientID%>").value;
+                if (sql.length == 0) {
+                    return false;
+                }
+            }
+
+            if (Text == 'Batch') {
+                var fileupload = document.getElementById("<%=FileUpload1.ClientID%>").value;
+                if (fileupload.length == 0) {
+                    return false;
+                }
+            }
+            if (Text != 'Query') {
+                return confirm('Are you sure to submit?');
+            }
+            else {
+                return true;
+            }
+        }
+       
+
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+      <input type="hidden" id="txtboxValue" />
+    <div style="width: 800px; margin: auto; font-weight: bold; font-size: 16px;">
+        Run Select ,Insert,Update,Delete, file(.sql) scripts
+       
+    </div>
+    <table class="tableborder" cellpadding="5" cellspacing="10">
+        <tr>
+            <td class="style1">
+                Type:
+            </td>
+            <td>
+                <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="true" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <asp:Panel ID="paneScripts" runat="server">
+            <tr>
+                <td class="style1">
+                    Script:
+                </td>
+                <td>
+                    <asp:TextBox ID="txtContent" runat="server" TextMode="MultiLine" CssClass="txtbox"
+                       Style="width: 620px; height: 300px;"></asp:TextBox>
+                </td>
+            </tr>
+        </asp:Panel>
+        <asp:Panel ID="panelFile" runat="server">
+            <tr>
+                <td class="style">
+                    file:
+                </td>
+                <td>
+                    <asp:FileUpload ID="FileUpload1" runat="server" />
+                </td>
+            </tr>
+        </asp:Panel>
+        <tr>
+            <td class="style1">
+            </td>
+            <td>
+                <div style="display:table-cell">
+                    <div style="display:inline-block">
+                        <asp:Button ID="btnRun" CssClass="btn-lit" runat="server" OnClientClick='return Submit();'
+                            Text="Run Script" OnClick="btnRun_Click" /></div>
+                    <div style="display:inline-block; padding-left:500px">
+                        <asp:Button ID="btnReset" CssClass="btn-lit" runat="server" Text="Clear" OnClick="btnReset_Click" /></div>
+                </div>
+            
+            </td>
+        </tr>
+        <tr>
+            <td>
+            </td>
+            <td>
+                <asp:Label ID="lblMsg" runat="server" Text="" ForeColor="Blue"></asp:Label>
+            </td>
+        </tr>
+    </table>
+    <br />
+    <asp:Panel ID="panelResults" runat="server">
+        <div style="width: 800px; margin: auto; overflow: auto;">
+            <h3>
+                Query Results:</h3>
+        </div>
+        <table class="tableborder" cellpadding="5" cellspacing="10">
+            <tr>
+                <td>
+                    Show Items:
+                    <asp:DropDownList ID="ddlItems" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlItems_SelectedIndexChanged">
+                    </asp:DropDownList>
+                </td>
+                <td>
+                    Export to Excel:
+                    <asp:ImageButton ID="imgExcel" runat="server" Height="19px" ImageUrl="~/Images/excel.gif"
+                        OnClick="imgExcel_Click" ToolTip="Export to Excel" Width="19px" />&nbsp;
+                </td>
+            </tr>
+        </table>
+    </asp:Panel>
+    <br />
+    <div style="width: 80%; margin: auto; overflow: auto;">
+        <asp:GridView ID="GridView1" runat="server" Width="100%" CssClass="GridRow1" HeaderStyle-CssClass="tableHeader"
+            AllowPaging="true" PageSize="20" OnPageIndexChanging="GridView1_PageIndexChanging">
+        </asp:GridView>
+    </div>
+    <br />
+    <br />
+    <br />
+    </form>
+</body>
+</html>
---
+++ c#/Files/RunScript.aspx.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Configuration;
+using VendorsEntity;
+using System.Data.OracleClient;
+using System.Data;
+using System.Data.Common;
+using System.IO;
+using System.Text;
+
+public partial class RunScript : System.Web.UI.Page
+{
+    string DDL_Batch = "Batch";
+    string DDL_Script = "UpdateOrInsert";
+    string DDL_Query = "Query";
+    string DDL_Item10 = "10";
+    string DDL_Item20 = "20";
+    string DDL_Item50 = "50";
+    string DDL_Item100 = "100";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            BindData();
+            InitControl();
+        }
+    }
+    private void BindData()
+    {
+        this.DropDownList1.Items.Clear();
+        this.DropDownList1.Items.Add(DDL_Query);
+        this.DropDownList1.Items.Add(DDL_Script);
+        this.DropDownList1.Items.Add(DDL_Batch);
+
+        this.ddlItems.Items.Clear();
+        this.ddlItems.Items.Add(DDL_Item10);
+        this.ddlItems.Items.Add(DDL_Item20);
+        this.ddlItems.Items.Add(DDL_Item50);
+        this.ddlItems.Items.Add(DDL_Item100);
+        this.ddlItems.SelectedIndex = 1;
+        
+    }
+    protected void btnReset_Click(object sender, EventArgs e)
+    {
+        this.txtContent.Text = string.Empty;
+        this.lblMsg.Text = string.Empty;
+        this.GridView1.DataSource = null;
+        this.GridView1.DataBind();
+        
+        Session["DS"] = null;
+    }
+    protected void btnRun_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            if (this.DropDownList1.SelectedItem.Text == DDL_Script)
+            {
+                string script = this.txtContent.Text.Trim();
+                if (script.Length == 0)
+                {
+                    ClientScript.RegisterStartupScript(ClientScript.GetType(), "Information", "<script>alert('Script cannot be empty.');</script>");
+                    return;
+                }
+                //run script
+                if (script.ToLower().IndexOf("update") < 0 && script.ToLower().IndexOf("insert") < 0 && script.ToLower().IndexOf("delete") < 0)
+                {
+                    ClientScript.RegisterStartupScript(ClientScript.GetType(), "Information", "<script>alert('Invalid Script.');</script>");
+                    return;
+                }
+                int i = RunScripts(script);
+                this.lblMsg.Text = "Total " + i + " rows affected";
+                ClientScript.RegisterStartupScript(ClientScript.GetType(), "Success", "<script>alert('script run successfully.');</script>");
+            }
+            else if (this.DropDownList1.SelectedItem.Text == DDL_Query)
+            {
+                string script = this.txtContent.Text.Trim();
+                if (script.Length == 0)
+                {
+                    ClientScript.RegisterStartupScript(ClientScript.GetType(), "Information", "<script>alert('Script cannot be empty.');</script>");
+                    return;
+                }
+                if (script.ToUpper().IndexOf("SELECT") < 0)
+                {
+                    ClientScript.RegisterStartupScript(ClientScript.GetType(), "Information", "<script>alert('Invalid Script.');</script>");
+                    return;
+                }
+                Session["DS"] = null;
+                BindQueryData();
+            }
+            else//run script in file
+            {
+                if (this.FileUpload1.HasFile)
+                {
+                    string fileName = this.FileUpload1.PostedFile.FileName;
+                    if (fileName.ToLower().IndexOf(".sql") <= 0)
+                    {
+                        ClientScript.RegisterStartupScript(ClientScript.GetType(), "Information", "<script>alert('File extension is not corrent');</script>");
+                        return;
+                    }
+                    string scripts = GetFileContentByName();
+                    int i = RunScripts(scripts);
+                    this.lblMsg.Text = "Total " + i + " rows affected";
+                    ClientScript.RegisterStartupScript(ClientScript.GetType(), "Success", "<script>alert('File script run successfully.');</script>");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            this.lblMsg.Text = ex.Message;
+            ClientScript.RegisterStartupScript(ClientScript.GetType(), "Error", "<script>alert('" + ex.Message + "');</script>");
+        }
+    }
+    private string GetFileContentByName()
+    {
+        StringBuilder sb = new StringBuilder();
+        try
+        {
+            using (Stream fileStream = FileUpload1.PostedFile.InputStream)
+            {
+                using (StreamReader sr = new StreamReader(fileStream))
+                {
+                    string idNum = null;
+                    while ((idNum = sr.ReadLine()) != null)
+                    {
+                        sb.Append(idNum);
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+
+    private int RunScripts(string scripts)
+    {
+        string Separator = ";";
+        Database db = null;
+        DbCommand dbCommand = null;
+        int count=0;
+        try
+        {
+            db = DatabaseFactory.CreateDatabase("AGD");
+            using (DbConnection connection = db.CreateConnection())
+            {
+                connection.Open();
+                DbTransaction transaction = connection.BeginTransaction();
+
+                try
+                {
+                    if (scripts.IndexOf(";") < 0)
+                    {
+                        scripts += Separator;
+                    }
+
+                    string[] array = scripts.Split(new string[] { Separator }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string sql in array)
+                    {
+                        if (sql.Trim().Length > 0)
+                        {
+                            dbCommand = db.GetSqlStringCommand(sql);
+                            int i = db.ExecuteNonQuery(dbCommand);
+                            count += i;
+                        }
+                    }
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw ex;
+                }
+                finally
+                {
+                    if (db != null) { db = null; }
+                    if (dbCommand != null) { dbCommand = null; }
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+            return count;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        InitControl();
+    }
+    private void InitControl()
+    {
+        if (DropDownList1.SelectedItem.Text == DDL_Script || DropDownList1.SelectedItem.Text == DDL_Query)
+        {
+            paneScripts.Visible = true;
+            panelFile.Visible = false;
+        }
+        else
+        {
+            paneScripts.Visible = false;
+            panelFile.Visible = true;
+        }
+        if (DropDownList1.SelectedItem.Text == DDL_Query)
+        {
+            panelResults.Visible = true;
+        }
+        else
+        {
+            panelResults.Visible = false;
+        }
+            
+        this.lblMsg.Text = string.Empty;
+    }
+
+    private void BindQueryData()
+    {
+        DataSet ds = new DataSet();
+        try
+        {
+            string sql = this.txtContent.Text.Trim();
+            sql=sql.Replace(";","");
+            if (Session["DS"] == null)
+            {
+                ds = GetDataset(sql);
+                Session["DS"] = ds;
+            }
+            else
+            {
+                ds = (DataSet)Session["DS"];
+            }
+            GridView1.PageSize = Convert.ToInt16(this.ddlItems.SelectedValue);
+            this.GridView1.DataSource = ds;
+            this.GridView1.DataBind();
+            this.lblMsg.Text = "Total " + ds.Tables[0].Rows.Count.ToString() + " Records Found.";
+            ds = null;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+    private DataSet GetDataset(string sql)
+    {
+        Database db = null;
+        DataSet ds = null;
+        DbCommand dbCommand = null;
+        try
+        {
+            db = DatabaseFactory.CreateDatabase("AGD");
+            dbCommand = db.GetSqlStringCommand(sql);
+            dbCommand.CommandType = CommandType.Text;
+            ds = new DataSet();
+            ds = db.ExecuteDataSet(dbCommand);
+            return ds;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        finally
+        {
+            if (db != null)
+                db = null;
+
+            if (dbCommand != null)
+                dbCommand = null;
+        }
+    }
+    protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        this.GridView1.PageIndex = e.NewPageIndex;
+        BindQueryData();
+    }
+
+    protected void imgExcel_Click(object sender, ImageClickEventArgs e)
+    {
+
+        try
+        {
+            Response.ClearHeaders();
+            DataSet ds = new DataSet();
+            if (this.GridView1.Rows.Count==0)
+            {
+                ClientScript.RegisterStartupScript(ClientScript.GetType(), "Warn", "<script>alert('No data to export.');</script>");
+                return;
+            }
+            if (Session["DS"]==null)
+            {
+                BindQueryData();
+            }
+            ds = (DataSet)Session["DS"];
+            DataTable dt = ds.Tables[0];
+
+            if (dt.Rows.Count == 0)
+            {
+                ClientScript.RegisterStartupScript(ClientScript.GetType(), "Warn", "<script>alert('No data to export.');</script>");
+                return;
+            }
+            string attachment = "attachment; filename=QueryResult.xls";
+            Response.ClearContent();
+            Response.AddHeader("content-disposition", attachment);
+            Response.ContentType = "application/vnd.ms-excel";
+            string tab = "";
+
+            foreach (DataColumn dc in dt.Columns)
+            {
+                Response.Write(tab + dc.ColumnName.ToUpper());
+                tab = "\t";
+            }
+            Response.Write("\n");
+            int i;
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                tab = "";
+                for (i = 0; i < dt.Columns.Count; i++)
+                {
+                    Response.Write(tab + dr[i].ToString());
+                    tab = "\t";
+                }
+                Response.Write("\n");
+            }
+            ds = null;
+            Response.End();
+        }
+        catch (Exception ex)
+        {
+            this.lblMsg.Text = ex.Message;
+            ClientScript.RegisterStartupScript(ClientScript.GetType(), "Error", "<script>alert('" + ex.Message + "');</script>");
+        }
+    }
+    protected void ddlItems_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (this.GridView1.Rows.Count > 0)
+        {
+            BindQueryData();
+        }
+    }
+}
