b324de74cf76405172fd6c94822eda484270250d
LiDeng <ChengduJackli@gmail.com>
2016-12-17 09:45:43 +0800

    CLA coding

---
+++ c#/CLRViaC#/CLAViaCSharp/CLAViaCSharp/Chapter8/Constructor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLAViaCSharp
+{
+    class Constructor
+    {
+        private Int32 m_x = 5;
+        public void Test()
+        {
+            Rectangle t = new Rectangle();
+
+            SomeType st = new SomeType();
+            st.run1();
+            st.run2();
+            SomeType.run3();
+        }
+    }
+    internal struct Point
+    {
+        public int m_x, m_y;
+        //结构中不能有实例字段初始值设定项
+        //private int m_z = 10;
+        public Point(int x, int y)
+        {
+            m_x = x;
+            m_y = y;
+            Console.WriteLine("m_x: " + m_x + " m_y:" + m_y);
+        }
+        //结构体不能包括无参构造器
+        //public Point()
+        //{
+        //    m_x = m_x = m_y = 5;
+        //}
+    }
+
+    internal sealed class Rectangle
+    {
+        public Point m_topLeft, m_bottomRight;
+        public Rectangle()
+        {
+        }
+    }
+
+    //静态构造器，类型构造器
+    internal class SomeType
+    {
+        //类型构造器是线程安全的，所以非常适合在类型构造器中初始化类型需要的任何单实例对象
+        static SomeType()
+        {
+            Console.WriteLine("Static sometype executed");
+        }
+
+        public void run1()
+        {
+            Console.WriteLine("SomeType.run1");
+        }
+        public void run2()
+        {
+            Console.WriteLine("SomeType.run2");
+        }
+        public static void run3()
+        {
+            Console.WriteLine("SomeType.run3");
+        }
+    }
+}
---
+++ c#/CLRViaC#/CLAViaCSharp/CLAViaCSharp/chapter17/deleteGate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CLAViaCSharp.chapter17
+{
+    internal delegate void Feedback(Int32 value);
+    internal delegate object MyCallback(FileStream fs);
+
+    public class deleteGate
+    {
+        public static void StaticDelegateDemo()
+        {
+            Counter(1, 3, null);
+            Counter(1, 3,new Feedback(FeedbackToConsole));
+            Counter(1, 3, FeedbackToConsole);
+
+
+            FileStream fs = new FileStream(@"C:\Users\lideng\Desktop\1.txt", FileMode.Append);
+            MyCallback m = new MyCallback(FileStr);
+            Console.WriteLine(m(fs));
+
+            ThreadPool.QueueUserWorkItem(new WaitCallback(FeedbackToConsole1),4);
+            ThreadPool.QueueUserWorkItem(FeedbackToConsole1, 5);
+            ThreadPool.QueueUserWorkItem(obj=>Console.WriteLine("Lambda:"+obj),10);
+
+            Func<Int32, string> f1 = (Int32 n) => n.ToString();
+            Console.WriteLine("f1:"+f1(123));
+        }
+
+        private static void Counter(Int32 from, Int32 to, Feedback fb)
+        {
+            for (Int32 val = from; val <= to; val++)
+            {
+                if (fb != null)
+                {
+                    fb(val);
+                }
+            }
+        }
+        private static void FeedbackToConsole1(object value)
+        {
+            Console.WriteLine("Item " + value);
+        }
+
+        private static void FeedbackToConsole(Int32 value)
+        {
+            Console.WriteLine("Item " + value);
+        }
+
+        private static string FileStr(Stream s)
+        {
+            return "123";
+        }
+    }
+
+    
+}
